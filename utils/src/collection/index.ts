6816aa5fc88a0573aa662adeb6f5afe1e64202ec
Netanel Basal <netanel7799@gmail.com>
2018-03-04 19:26:47 +0200

    Remove camelCase kebabCase Capitilaize and size

--- utils/src/collection/index.ts
+++ utils/src/collection/index.ts
@@ -7,6 +7,8 @@ import _reject from 'lodash.reject';
 import _groupBy from 'lodash.groupby';
 import _filter from 'lodash.filter';
 import _size from 'lodash.size';
+import { toBoolean } from '../coercion';
+import {isObject} from '../validators';
 
 /**
  *
@@ -89,7 +91,15 @@ export function filter(collection, predicate?) {
  * @param collection
  */
 export function size(collection) {
-  return _size(collection);
+  if(Array.isArray(collection)) {
+    return collection.length;
+  }
+
+  if(isObject(collection)) {
+    return Object.keys(collection).length;
+  }
+
+  return toBoolean(collection) ? collection.length : 0;
 }
 
 /**
