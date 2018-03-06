fe5fb348c2985b670072dbea203eec5e6677802e
Amit Bh <amit@datorama.com>
2018-03-06 17:56:37 +0200

    - Refactor core - Add a CSS fallback for legacy browsers. - Add support in pseudo classes.

--- themes-playground/gulpfile.js
+++ themes-playground/gulpfile.js
@@ -2,13 +2,28 @@ const rename = require("gulp-rename");
 const sass = require('postcss-node-sass');
 const postcss = require('gulp-postcss');
 const gulp = require('gulp');
-const themify = require('./core/dato-themes');
+const {initThemify, themify} = require('./core/themify');
+const path = require('path');
+
+const pallete = require(path.join(__dirname, './src/theme/pallete.ts'));
+
+const themifyOptions = {
+  createVars: true,
+  pallete: pallete,
+  screwIE11: false,
+  fallback: {
+    cssPath: path.join(__dirname, './dist/theme_fallback.css'),
+    dynamicPath: path.join(__dirname, './dist/theme_fallback.json')
+  }
+};
 
 gulp.task('default', () => {
+
   return gulp.src('./src/index.themes.scss')
     .pipe(postcss([
+      initThemify(themifyOptions),
       sass(),
-      themify()
+      themify(themifyOptions)
     ]))
     .pipe(rename("bundle.css"))
     .pipe(gulp.dest('dist'));
