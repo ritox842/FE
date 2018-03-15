5d22f4fda04bff932648a3ff5b0903bfad9ad664
Inbal Sinai <inbal.sinai@datorama.com>
2018-03-15 10:58:14 +0200

    rounding function

---
+++ utils/src/number/index.ts
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param num - the number to round
+ * @param precision - how many placed after the decimal point to retain (default: 2, i.e. to the hundredth decimal)
+ * @returns {number} - the rounded number
+ */
+export function roundToDecimal(num, precision) {
+    if (isNaN(num)) {
+        throw 'value to round is not a number';
+    }
+    if (precision && isNaN(precision)) {
+        throw 'precision value is not a number';
+    }
+    const factor = precision ? Math.pow(10, precision) : Math.pow(10, 2);
+    return Math.round(num * factor) / factor;
+}
